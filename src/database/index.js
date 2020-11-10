61150454e826bd769bfbb372ea43ae93c0065eab
viniguidotti <vinimoraes321@hotmail.com>
2020-11-10 10:44:46 -0300

    change uri

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb+srv://vini:vini2308@vinim.695dm.mongodb.net/admin?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://vini:vini2308@vinim.695dm.mongodb.net/crudkruzer?retryWrites=true&w=majority')
 // mongoose.connect('mongodb://localhost/crudkruzer');
 mongoose.Promise = global.Promise;
 
