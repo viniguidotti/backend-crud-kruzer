4d39a32f0f5325ee0afc57d8381da32dbe059d21
Vinicius Guidotti Moraes <vinicius@zapway.com.br>
2021-04-10 15:49:55 -0300

    remove nodemodules and added gitignore

--- src/database/index.js
+++ src/database/index.js
@@ -5,8 +5,8 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb+srv://vini:vini2308@vinim.695dm.mongodb.net/crudkruzer?retryWrites=true&w=majority')
-// mongoose.connect('mongodb://localhost/crudkruzer');
+// mongoose.connect('mongodb+srv://vini:vini2308@vinim.695dm.mongodb.net/crudkruzer?retryWrites=true&w=majority')
+mongoose.connect('mongodb://localhost/crudkruzer');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
